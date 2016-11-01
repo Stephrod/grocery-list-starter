4ae20f606704cc7fd82ee6c93048ffe9d94bb17d
Stephrod <stephanie_rodriguez5@yahoo.com>
2016-10-31 23:56:30 -0500

    started framework for app

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/grocery-list-starter');
 
-app.user(bodyParser.json());
-app.user(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const port = 3000;
 app.listen(port, () => console.log(`Listening on ${port}`));
