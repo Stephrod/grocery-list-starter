166c07733a54c48204f469b900bcb25ae6ed1a3d
Stephrod <stephanie_rodriguez5@yahoo.com>
2016-11-01 20:20:47 -0500

    Able to add grocery item and quantity in app

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+const ejs = require('ejs');
 
 const app = express();
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/grocery-list-starter');
 
+const TaskModel = require('./models/TaskModel.js')
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res, next) => {
+  TaskModel.find((err, tasks) => {
+    // tasks === [{}, {}, {}]
+    res.render('index', { tasks: tasks });
+  });
+});
+
+app.post('/tasks', (req, res, next) => {
+  var task = new TaskModel({
+    name : req.body.name,
+    quantity : req.body.quantity
+  });
+
+  task.save((err, task) => {
+    res.redirect('/');
+  });
+});
 
 const port = 3000;
 app.listen(port, () => console.log(`Listening on ${port}`));
--- models/TaskModel.js
+++ models/TaskModel.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const taskSchema = new Schema({
-
+  'name': String,
+  'quantity': Number
 });
 
-model.exports = mongoose.model('name', taskSchema)
+module.exports = mongoose.model('tasks', taskSchema)
 ;
