cd6328706b82c70d39c56f44165b73fce4592b2b
Stephrod <stephanie_rodriguez5@yahoo.com>
2016-11-06 21:55:32 -0600

    update to controller file

--- controllers/GroceryController.js
+++ controllers/GroceryController.js
@@ -6,42 +6,39 @@ mongoose.Promise = global.Promise;
 const GroceryController = {
 
 list: function(req, res, next) {
-  TaskModel.find('tasks').exec()
-  .then(tasks => res.render('index', {tasks}))
+  TaskModel.find.exec()
+  .then(tasks => res.json(tasks))
   .catch(err => next(err))
 },
 
 show: function(req, res, next) {
   TaskModel.findById(req.params.id).exec()
-  .then(match => res.render('index', {match}))
-  .catch(err => next(err))
+  .then(tasks => res.json(tasks)
+  .catch(err => next(err));
 },
 
 create: function(req, res, next) {
-  const task = new TaskModel({
-    text: req.body.text,
-    quantity: req.body.quantity
+  new TaskModel({text: req.body.text, quantity: req.body.quantity}).save()
 });
-  task.save()
-  .then(task => res.redirect('index'))
-  .catch(err => next(err))
+  .then(task => res.json(tasks))
+  .catch(err => next(err));
 },
 
 update: function(req, res, next) {
-  TaskModel.findByIdAndUpdate({ _id: req.params.id }, {
-    text: req.body.text,
-    quantity: rexq.body.quantity
-  }, { new: true }).exec()
-    .then(tasks => res.render(tasks))
-    .catch(err => next(err))
+  TaskModel.findById(req.params.id }.exec()
+    .then(tasks => {
+      text: req.body.text,
+      quantity: req.body.quantity,
+      return tasks.save();
+    })
+    .then(tasks => res.json(tasks))
+    .catch(err => next(err));
 },
 
 remove: function(req, res, next) {
-  const id = req.params.id
-  TaskModel.findByIdAndRemove({_id: id}).exec()
-  .then(tasks => res.render(tasks))
-  .catch(err => next(err))
-  }
+  TaskModel.findByIdAndRemove(req,params.id).exec()
+  .then(tasks => res.json(tasks))
+  .catch(err => next(err));
 };
 
 module.exports = GroceryController
